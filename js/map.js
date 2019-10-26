4f971fc4d2d5cf8512ab8db6837bc8e659e42de3
ysoh <juliepilarski@yahoo.fr>
2019-10-27 01:36:37 +0200

    Création class Map

--- js/map.js
+++ js/map.js
@@ -1,22 +1,23 @@
-//Initialisation de la latitude et de la longitude (ville: Nante)
-var lat = 47.218371;
-var lon = -1.553621;
-var mymap = null;
+class Map {
+    constructor(lat, lon) {
+        this.lat = lat;
+        this.lon = lon;
+        this.map = document.getElementById('mapid');
+    }
 
-//Initialisation de la carte
-function initMap() {
-    mymap = L.map('mapid').setView([lat, lon], 13);
+    initMap() {
+        this.map = L.map(this.map).setView([this.lat, this.lon], 15);
 
-    //Récupération du "tiles"
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        minZoom: 1,
-        maxZoom: 20,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoieXNvbGluZSIsImEiOiJjazF4dmY0bXgwZmZpM21sOGlzOTN4aGszIn0.YBzO02ZF423AzJFxbjJIkg'
-    }).addTo(mymap);
+        this.layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            minZoom: 1,
+            maxZoom: 20,
+            id: 'mapbox.streets',
+            accessToken: 'pk.eyJ1IjoieXNvbGluZSIsImEiOiJjazF4dmY0bXgwZmZpM21sOGlzOTN4aGszIn0.YBzO02ZF423AzJFxbjJIkg'
+        }).addTo(this.map);
+
+    }
 }
 
-window.onload = function () {
-    initMap();
-};
+const myMap = new Map(47.21837, -1.553621);
+myMap.initMap();
