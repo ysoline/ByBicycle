58b5278249612fe3e9395ff91f2b1b89eb0d637a
ysoh <juliepilarski@yahoo.fr>
2019-11-18 18:18:45 +0100

    Création class

---
+++ js/Class/Bookings.js
@@ -0,0 +1,16 @@
+class Bookings {
+    constructor() {
+        this.timer = timer;
+    }
+
+    detectBooking() {
+        //activation quand le client "signe" la réservation
+    }
+    timer() {
+        //décompte 20min et expiration
+    }
+    viewTimer() {
+        //affichage sous carte du minuteur (coloration lorsqu'il reste moins de 5min)
+    }
+
+}
