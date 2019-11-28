90cb5c4936fcac8e27b103f5564f21e09a9404f0
Julie <juliepilarski@yahoo.fr>
2019-11-28 13:27:12 +0100

    mise à jour

--- js/Class/Canvas.js
+++ js/Class/Canvas.js
@@ -5,66 +5,35 @@ class Canvas {
         this.ctx = this.c.getContext('2d')
         this.ctx.fillStyle = "black"
         this.clear = document.getElementById('clear_canvas')
+        this.drawMouse()
 
-        this.sign()
         this.clearCanvas()
     }
 
 
-
-    sign() {
+    drawMouse() {
         this.c.addEventListener('mousemove', (e) => {
             if (e.buttons === 1) {
-                this.mousePosition(e)
-                this.draw()
+                this.ctx.beginPath()
+                this.ctx.arc(e.offsetX, e.offsetY, 1, 0, Math.PI * 2)
+                this.ctx.stroke()
+                this.ctx.fillStyle
+                this.ctx.fill()
             }
         })
-
-        this.c.addEventListener('touchmove', (e) => {
-            if (e.toucheStart === 1) {
-                this.fingerPosition(e)
-                this.draw()
-            }
-        })
-
-    }
-    mousePosition(e) {
-
-        this.x = e.offsetX
-        this.y = e.offsetY
-    }
-    fingerPosition(e) {
-        this.x = e.touches[0].offsetX
-        this.y = e.touches[0].offsetY
-
+        // this.c.addEventListener('mouseup', (e) => {
+        //     if (e.buttons === 0) {
+        //         e.stopPropagation()
+        //     }
+        // })
     }
 
 
-    draw() {
-        this.ctx.beginPath()
-        this.ctx.arc(this.x, this.y, 1, 0, Math.PI * 2)
-        this.ctx.stroke()
-        this.ctx.fillStyle
-        this.ctx.fill()
-    }
-
-
-    clearCanvas() {
-        this.clear.addEventListener('click', (e) => {
-            e.preventDefault()
+    clearCanvas(e) {
+        this.clear.addEventListener('click', () => {
+            
             this.ctx.clearRect(0, 0, this.c.width, this.c.height)
         })
     }
 
-    saveCanvas() {
-        //Méthode permettant de sauvegarder le canva sous forme d'image afin de pouvoir l'exploiter ensuite.
-    }
-
 }
-
-//position: récupération des coordonnées de la souris
-//Draw : methode regroupant les paramètres pour dessiner (arc,stroke...)
-//MouseDown : si souris enfoucé (detectée), appel de la méthode mouseMove + draw + position
-//MouseUp + Out: return false (ou 0) pour draw, ne dessine plus quand clique relacher ou sortie de la fenétre
-//ClearCanva : réinitialise le canva, pour effacement
-//Tablette/Phone : toucheStart, touchMove, touchEnd(?) => même principe que mouse
