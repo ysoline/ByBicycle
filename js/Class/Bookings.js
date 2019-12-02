11f068a1b8cc4a36af59c87151e7920da6790837
ysoh <juliepilarski@yahoo.fr>
2019-12-02 07:22:20 +0100

    Adaptation du timer sur formulaire

--- js/Class/Bookings.js
+++ js/Class/Bookings.js
@@ -2,64 +2,74 @@
 //Les infos sauvargés sont : le timer, le nom de la station où est effectué la réservation
 
 class Bookings {
-  constructor(nb) {
-    this.timerView = document.getElementById("timer")
-    this.contentBooking = document.getElementById('content_booking')
-    this.min = nb
-    this.sec = nb * 60
-    this.min = 0
-    this.remainingSec = 0
-    this.stopTimer()
-    this.detectBooking()
-  }
+    constructor() {
+        this.timerView = document.getElementById("timer")
+        this.contentBooking = document.getElementById('content_booking')
 
-  detectBooking() {
-    //s'active quand le client "signe" la réservation  
-    this.timerView.innerHTML = 'Expiration de la réservation dans ' + this.min + ':' + this.remainingSec
-    this.interval = setInterval(this.startTimer.bind(this), 1000) //S'execute toutes les secondes
-  }
+        this.nb = 1 //Temps pour réservation (20min)
+        this.min = 0
+        this.remainingSec = 0
+        this.stopTimer()
 
-  startTimer() { //Décrémentation
+        if (sessionStorage.getItem('timer')) {
 
-    this.min = Math.round((this.sec - 30) / 60)
-    this.remainingSec = this.sec % 60
+            document.getElementById('current_booking').style.display = 'block'
+            document.getElementById('booking_info').innerHTML = 'Vélo réservé à la station' + sessionStorage.getItem('stationName') + ' par ' + localStorage.getItem('firstname') + ' ' + localStorage.getItem('name')
 
-    if (this.remainingSec < 10) {
-      this.remainingSec = '0' + this.remainingSec
+            this.sec = sessionStorage.getItem('timer')
+            this.detectBooking()
+
+        } else {
+            this.sec = this.nb * 60
+        }
     }
-    this.timerView.innerHTML = 'Expiration de la réservation dans ' + this.min + ':' + this.remainingSec
 
-    if (this.sec == 0) {
-      this.stopTimer()
-      this.contentBooking.innerHTML = '<span class="booking_over">Réservation expirée</span>'
-    } else {
-      this.sec--
+    detectBooking() {
+        //s'active quand le client "signe" la réservation  
+        this.timerView.innerHTML = 'Temps restant : ' + this.min + ':' + this.remainingSec
+        this.interval = setInterval(this.startTimer.bind(this), 1000) //S'execute toutes les secondes
+
     }
-  }
 
-  stopTimer() { //Stop le timer 
+    startTimer() { //Décrémentation
 
-    clearInterval(this.interval)
-    this.clearStorage()
-  }
+        this.min = Math.round((this.sec - 30) / 60)
+        this.remainingSec = this.sec % 60
 
-  checkStorage() {
-    //Méthode pour vérifier si objet de stockage existent ou non
-    if (sessionStorage.getItem("timer")) {
-      this.updateInfo()
-    } else {
-      this.setInfo()
+        if (this.remainingSec < 10) {
+            this.remainingSec = '0' + this.remainingSec
+        }
+
+        this.timerView.innerHTML = 'Temps restant : ' + this.min + 'min ' + this.remainingSec + 's'
+        this.saveInfo()
+
+        if (this.sec == 0) {
+            this.stopTimer()
+            this.timerView.innerHTML = '<span class="booking_over">Réservation expirée</span>'
+            this.clearStorage()
+            this.refreshPage()
+        } else {
+            this.sec--
+        }
     }
-  }
-  updateInfo() {
-    sessionStorage.get('timer')
 
-  }
-  saveInfo() {
-    sessionStorage.setItem('timer', this.nb)
-    console.log(this.s + " SAUVEGARDE")
-  }
-  clearStorage() {
-    sessionStorage.clear()
-  }
+    stopTimer() { //Stop le timer 
+        clearInterval(this.interval)
+    }
+
+
+    saveInfo() { //Sauvegarde le temps restant
+        sessionStorage.setItem('timer', this.sec)
+    }
+
+    clearStorage() { //Détruit l'objet storage une fois le temps écoulé
+        sessionStorage.removeItem('timer')
+    }
+
+    refreshPage() { //Lorsqu'une réservation est finie, afin de permettre une nouvelle réservation
+        console.log('Refresh de la page dans 30s')
+        setTimeout(() => {
+            document.location.reload(true)
+        }, 30000)
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
+//Cette classe permet de gérée l'ensemble des autres classes avec la création d'objet.
+
 class App {
   constructor() {
-    this.newMap = new Map(47.21837, -1.55624, mapid);
+    this.newMap = new Map(47.21837, -1.55624, mapid); //Défini sur Nantes
+    this.mySlider = new Slider('diapo', images, 'img/', 5000);
     this.getAjax(this.newMap);
     this.getBooking();
-    this.btnTimer = document.getElementById("goTimer");
+    this.listenSlider()
   }
-  getAjax(newMap) {
+  getAjax(newMap) { //Récupération des informations de JCDecaux
     ajaxGet(
       "https://api.jcdecaux.com/vls/v1/stations?contract=nantes&apiKey=a0ce65c3665035a0a4c7ef02805b70e171c87636",
       function (response) {
@@ -29,36 +32,83 @@ class App {
       }
     );
   }
-  getBooking() {
-    let signature = new Canvas();
-    let showForm = document.getElementById("showBooking");
-    let client = new Client();
+  listenSlider() {
+    //interraction : bouton cliquer
+    $('#pauseBtn').on('click', () => {
+      this.mySlider.pause();
+    });
+    $('#nextBtn').on('click', () => {
+      this.mySlider.next();
+    });
+    $('#prevBtn').on('click', () => {
+      this.mySlider.preview();
+    });
 
+    document.addEventListener('keydown', (e) => {
+      this.mySlider.pressKeyboard(e)
+    })
+
+    document.getElementById('name').addEventListener('focus', () => {
+
+    })
+
+  }
+  getBooking() { //Réservation 
+    this.mySlider.keyboardIsActive = false
+
+    let showForm = document.getElementById("showBooking")
+
+    let signature = new Canvas()
+    let client = new Client()
+    let timer = new Bookings()
+
+    //Apparation du formulaire de réservation lors du click sur le bouton "Faire une réservation"
     showForm.addEventListener("click", () => {
+
       showForm.style.display = "none";
       document.getElementById("booking").style.display = "block";
+
     });
 
+    //Permet d'effacer les informations du formulaire de réservation
+    document.getElementById('erase').addEventListener('click', (e) => {
+      client.resetValue()
+    })
+
+    document.getElementById('sign_btn').addEventListener("click", () => {
+      document.getElementById('sign_form').style.display = 'block'
+    });
+
+    //Enregistrement d'une réservation quand conditions remplies
     document.getElementById('booking').addEventListener('submit', (e) => {
+
       e.preventDefault();
 
-      if (client.info()) {
+      //Action de vérifié les informations saisies du formulaire
+      client.info()
 
-        this.stationName = document.getElementById('station_name').innerHTML
-        this.stationAddr = document.getElementById('station_address').innerHTML
-        this.stationNbBike = document.getElementById('nb_bike').innerHTML
+      //Si le formulaire de réservation renvoie TRUE
+      if (client.info() == true) {
 
-        let timer = new Bookings(20); //Saisir un temps en minutes
-        document.getElementById('current_booking').style.display = 'block'
+        //Vérifie si un dessin est créé
+        if (signature.isEmpty == false) {
+          document.getElementById('current_booking').style.display = 'block'
 
-        //Retourne les infos lié à la réservation, enlève 1 vélo sur le nombre de vélos disponible à la station selectionnée
-        document.getElementById('booking_name').innerHTML = this.stationName
-        document.getElementById('booking_addr').innerHTML = this.stationAddr
-        this.stationNbBike = parseInt(this.stationNbBike - 1)
-        document.getElementById('nb_bike').innerHTML = this.stationNbBike
+          //Sauvegarde le nom de la station lors de la réservation
+          this.stationName = document.getElementById('station_name')
+          sessionStorage.setItem('stationName', this.stationName.innerHTML)
+
+
+          document.getElementById('booking_info').innerHTML = 'Vélo réservé à la station' + ' ' + sessionStorage.getItem('stationName') + ' par ' + localStorage.getItem('firstname') + ' ' + localStorage.getItem('name')
+
+          document.getElementById('error_sign').innerHTML = ''
+          signature.toImg() //Sauvegarde la signature sous format image 
+          //Démarrage du timer
+          timer.detectBooking()
+        } else {
+          document.getElementById('error_sign').innerHTML = 'Veuillez signer'
+        }
       }
-    });
-
-
+    })
   }
 }
