40ef9f5f6ad35c02e99b8c05efcd87c45dc83655
ysoh <juliepilarski@yahoo.fr>
2019-12-02 07:26:34 +0100

    Variable pour détecté canvas vide

--- js/Class/Canvas.js
+++ js/Class/Canvas.js
@@ -4,20 +4,20 @@ class Canvas {
         this.ctx = this.c.getContext('2d')
         this.ctx.fillStyle = "black"
         this.clear = document.getElementById('clear_canvas')
-        this.dataUrl = null
         this.sign()
         this.clearCanvas()
         this.ctx.save() //Sauvegarde l'état du canvas
+        this.isEmpty = true
     }
 
 
-
+    //Action de signer le canvas, souris ou doigt
     sign() {
         this.c.addEventListener('mousemove', (e) => {
             if (e.buttons === 1) {
                 this.mousePosition(e)
                 this.draw()
-                this.saveCanvas()
+                this.isEmpty = false
             }
         })
 
@@ -25,23 +25,26 @@ class Canvas {
             if (e.toucheStart === 1) {
                 this.fingerPosition(e)
                 this.draw()
+                this.isEmpty = false
             }
         })
+
     }
 
+    //Récupère la position de la souris
     mousePosition(e) {
-
         this.x = e.offsetX
         this.y = e.offsetY
     }
 
+    //Récupère la position de l'écran
     fingerPosition(e) {
         this.x = e.touches[0].offsetX
         this.y = e.touches[0].offsetY
 
     }
 
-
+    //Initialise le dessin
     draw() {
         this.ctx.beginPath()
         this.ctx.arc(this.x, this.y, 1, 0, Math.PI * 2)
@@ -50,19 +53,23 @@ class Canvas {
         this.ctx.fill()
     }
 
-
+    //Efface le dessin du canva
     clearCanvas() {
         this.clear.addEventListener('click', (e) => {
             e.preventDefault()
             this.ctx.clearRect(0, 0, this.c.width, this.c.height)
+            this.isEmpty = true
         })
     }
 
-}
+    //Sauvagarde le canvas en image
+    toImg() {
+        if (!!empty == false) {
+            let dataUrl = this.c.toDataURL('sign/png', 0.5);
+            console.log(dataUrl)
+        } else {
+            console.log('Veuillez signer le formulaire')
+        }
+    }
 
-//position: récupération des coordonnées de la souris
-//Draw : methode regroupant les paramètres pour dessiner (arc,stroke...)
-//MouseDown : si souris enfoucé (detectée), appel de la méthode mouseMove + draw + position
-//MouseUp + Out: return false (ou 0) pour draw, ne dessine plus quand clique relacher ou sortie de la fenétre
-//ClearCanva : réinitialise le canva, pour effacement
-//Tablette/Phone : toucheStart, touchMove, touchEnd(?) => même principe que mouse
+}
